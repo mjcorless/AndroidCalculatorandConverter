40e2bfd023d89486ecd7b534e4090d46f01739a1
mjcorless <mjcorles@asu.edu>
2017-12-16 00:45:54 -0700

    Initial Commit: Navigation Drawer and minimal app layout

---
+++ app/src/main/java/corless/matthew/devcalculatorandconverter/BaseActivity.java
@@ -0,0 +1,109 @@
+package corless.matthew.devcalculatorandconverter;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class BaseActivity extends AppCompatActivity
+		implements NavigationView.OnNavigationItemSelectedListener
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_base);
+		Toolbar toolbar = findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
+
+
+		DrawerLayout drawer = findViewById(R.id.drawer_layout);
+		ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+				this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+		drawer.addDrawerListener(toggle);
+		toggle.syncState();
+
+		NavigationView navigationView = findViewById(R.id.nav_view);
+		navigationView.setNavigationItemSelectedListener(this);
+	}
+
+	@Override
+	public void onBackPressed()
+	{
+		DrawerLayout drawer = findViewById(R.id.drawer_layout);
+		if (drawer.isDrawerOpen(GravityCompat.START))
+		{
+			drawer.closeDrawer(GravityCompat.START);
+		}
+		else
+		{
+			super.onBackPressed();
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public boolean onNavigationItemSelected(MenuItem item)
+	{
+		// Handle navigation view item clicks here.
+		int id = item.getItemId();
+		//Intent intent = null;
+		Intent intent = new Intent(this, NumericalConverterActivity.class);
+
+
+		if (id == R.id.nav_converter)
+		{
+			intent = new Intent(this, NumericalConverterActivity.class);
+		}/*
+		else if (id == R.id.nav_gallery)
+		{
+
+		}
+		else if (id == R.id.nav_slideshow)
+		{
+
+		}
+		else if (id == R.id.nav_manage)
+		{
+
+		}
+		else if (id == R.id.nav_share)
+		{
+
+		}
+		else if (id == R.id.nav_send)
+		{
+
+		}*/
+
+		startActivity(intent);
+		DrawerLayout drawer = findViewById(R.id.drawer_layout);
+		drawer.closeDrawer(GravityCompat.START);
+		return true;
+	}
+}
